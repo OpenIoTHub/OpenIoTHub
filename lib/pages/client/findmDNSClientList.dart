b163baf7c078d87e4209efb79b59a616e63059e9
Farry <yu@iotserv.com>
2019-07-18 13:58:19 +0800

    添加配置远程内网端的作用

--- lib/pages/client/findmDNSClientList.dart
+++ lib/pages/client/findmDNSClientList.dart
@@ -1,13 +1,10 @@
-import 'package:android_intent/android_intent.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:nat_explorer/api/Utils.dart';
+import 'package:nat_explorer/pages/client/setClient.dart';
 import 'package:nat_explorer/pb/service.pb.dart';
 import 'package:nat_explorer/pb/service.pbgrpc.dart';
-import 'package:grpc/grpc.dart';
-import 'package:nat_explorer/api/SessionApi.dart';
 
-const String discovery_service = "_http._tcp";
+const String discovery_service = "_nat-cloud-client._tcp";
 
 class FindmDNSClientListPage extends StatefulWidget {
   FindmDNSClientListPage({Key key}) : super(key: key);
@@ -48,7 +45,7 @@ class _FindmDNSClientListPageState extends State<FindmDNSClientListPage> {
               ),
               Expanded(
                   child: Text(
-                    '${pair}',
+                    '${pair.name}@${pair.iP}:${pair.port}',
                 style: _biggerFont,
               )),
               rightArrowIcon
@@ -59,9 +56,11 @@ class _FindmDNSClientListPageState extends State<FindmDNSClientListPage> {
           onTap: () {
             //直接打开内置web浏览器浏览页面
             Navigator.of(context)
-                .push(new MaterialPageRoute(builder: (context) {
-              return Text("${pair.iP}:${pair.port}");
-            }));
+                .push(MaterialPageRoute(builder: (context) {
+//              return Text("${pair.iP}:${pair.port}");
+              return SetClient(ip:pair.iP,port:pair.port);
+            }
+            ));
           },
           child: listItemContent,
         );
@@ -73,7 +72,7 @@ class _FindmDNSClientListPageState extends State<FindmDNSClientListPage> {
     ).toList();
     return Scaffold(
       appBar: AppBar(
-        title: Text("服务列表"),
+        title: Text("内网端列表"),
         actions: <Widget>[
           IconButton(
               icon: Icon(
@@ -83,72 +82,18 @@ class _FindmDNSClientListPageState extends State<FindmDNSClientListPage> {
               onPressed: () {
                 _findClientListBymDNS();
               }),
-          IconButton(
-              icon: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-              onPressed: () {
-
-              }),
-          IconButton(
-              icon: Icon(
-                Icons.info,
-                color: Colors.white,
-              ),
-              onPressed: () {
-
-              }),
         ],
       ),
       body: ListView(children: divided),
     );
   }
 
-  void _pushDetail(SessionConfig config) async {
-//:TODO    这里显示内网的服务，socks5等，右上角详情才展示详细信息
-    final List _result = [];
-    _result.add("ID:${config.runId}");
-    _result.add("描述:${config.description}");
-    _result.add("连接码:${config.token}");
-    _result.add("转发连接状态:${config.statusToClient ? "在线" : "离线"}");
-    _result.add(
-        "P2P连接状态:${config.statusP2PAsClient || config.statusP2PAsServer ? "在线" : "离线"}");
-    await Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) {
-          final tiles = _result.map(
-            (pair) {
-              return ListTile(
-                title: Text(
-                  pair,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('网络详情'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
-
   void _findClientListBymDNS() async {
     MDNSService config = MDNSService();
 //    config.name = '_nat-cloud-client._tcp';
-    config.name = '_http._tcp';
-    setState(() {
+    config.name = discovery_service;
       UtilApi.getAllmDNSServiceList(config).then((v){
+        setState(() {
         _ServiceList = v.mDNSServices;
       });
     });
---
+++ lib/pages/client/setClient.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class SetClient extends StatefulWidget {
+  SetClient({Key key, this.ip, this.port}) : super(key: key);
+
+  String ip;
+  int port;
+
+  @override
+  createState() => SetClientState();
+}
+
+class SetClientState extends State<SetClient> {
+  TextEditingController _host_controller = TextEditingController.fromValue(
+      TextEditingValue(text: "guonei.nat-cloud.com"));
+  TextEditingController _tcp_port_controller =
+      TextEditingController.fromValue(TextEditingValue(text: "34320"));
+  TextEditingController _udp_p2p_controller =
+      TextEditingController.fromValue(TextEditingValue(text: "34321"));
+  TextEditingController _key_controller =
+      TextEditingController.fromValue(TextEditingValue(text: "HLLdsa544&*S"));
+  TextEditingController _runid_controller =
+      TextEditingController.fromValue(TextEditingValue(text: ""));
+
+  login() async {
+    var url = 'http://${widget.ip}:${widget.port}/loginServer';
+    var response;
+    try {
+      var config = {
+        'last_id': _runid_controller.text,
+        'server_host': _host_controller.text,
+        'tcp_port': _tcp_port_controller.text,
+        'udp_p2p_port': _udp_p2p_controller.text,
+        'login_key': _key_controller.text
+      };
+      response = await http.post(url, body: config);
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        if (data['Code'] == 0) {
+          showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                      title: Text("登录结果"),
+                      content: Text("登录成功！现在可以获取访问Token来访问本内网了！"),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text("取消"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                          child: Text("确认"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        )
+                      ]));
+        } else {
+          showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                      title: Text("登录结果"),
+                      content: Text("登录失败：" + data['Msg'].toString()),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text("取消"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                          child: Text("确认"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        )
+                      ]));
+        }
+      }
+    } catch (exception) {
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                  title: Text("登录服务器错误"),
+                  content: Text(exception.toString()),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text("取消"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    FlatButton(
+                      child: Text("确认"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ]));
+    }
+  }
+
+  seeToken() async {
+    var url = 'http://${widget.ip}:${widget.port}/getExplorerToken';
+    var response;
+    try {
+      response = await http.get(url);
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        if (data['Code'] == 0) {
+          showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                      title: Text("本内网访问Token"),
+                      content: TextFormField(initialValue: data['Token']),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text("取消"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                          child: Text("复制到剪切板"),
+                          onPressed: () {
+                            Clipboard.setData(
+                                ClipboardData(text: data['Token']));
+                            Navigator.of(context).pop();
+                          },
+                        )
+                      ]));
+        }
+      }
+    } catch (e) {
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                  title: Text("获取Token出现错误！"),
+                  content: Text(e.toString()),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text("取消"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    FlatButton(
+                      child: Text("确认"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ]));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("内网穿透(内网端)"),
+        ),
+        body: ListView(
+          children: <Widget>[
+            TextFormField(
+              controller: _host_controller,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10.0),
+                labelText: '请输入服务器地址',
+                helperText: '输入ip或者域名',
+              ),
+            ),
+            TextFormField(
+              controller: _tcp_port_controller,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10.0),
+                labelText: '请输入服务器TCP端口',
+                helperText: '与服务器的设置一致：tcp_port',
+              ),
+            ),
+            TextFormField(
+              controller: _udp_p2p_controller,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10.0),
+                labelText: '请输入服务器udp_p2p端口',
+                helperText: '请输入服务器设置一致：udp_p2p_port',
+              ),
+            ),
+            TextFormField(
+              controller: _key_controller,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10.0),
+                labelText: '请输入服务器秘钥',
+                helperText: '请输入服务器设置一致：login_key',
+              ),
+            ),
+            TextFormField(
+              controller: _runid_controller,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10.0),
+                labelText: '请输入本内网id',
+                helperText: '可以随机输入一个UUID，可以留空随机生成',
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  child: RaisedButton(
+                    child: Text("连接服务器"),
+                    color: Colors.blue,
+                    onPressed: () {
+//                      从用户的填写中获取参数请求后端连接服务器
+                      login();
+                    },
+                  ),
+                  margin: EdgeInsets.all(10.0),
+                ),
+                RaisedButton(
+                    child: Text("查看Token"),
+                    color: Colors.green,
+                    onPressed: () {
+                      seeToken();
+                    })
+              ],
+            )
+          ],
+        ));
+  }
+}
