17ed6e63bdaec8773fe9fef9d265cf316c7a22fe
Farry <yu@iotserv.com>
2019-08-01 12:11:04 +0800

    添加运行新进程的例子

--- lib/constants/Config.dart
+++ lib/constants/Config.dart
@@ -3,8 +3,8 @@ class Config {
   static final String webStaticIp = "127.0.0.1";
   static final int webStaticPort = 9000;
 
-//  static final String webgRpcIp = "127.0.0.1";
   static final String webgRpcIp = "127.0.0.1";
+//  static final String webgRpcIp = "192.168.1.102";
   static final int webgRpcPort = 2080;
   static final int webRestfulPort = 1081;
 }
