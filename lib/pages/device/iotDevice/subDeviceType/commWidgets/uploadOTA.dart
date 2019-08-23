364ca67f115507d29e3455a0ad953158c6b57601
Farry <newfarry@126.com>
2019-08-24 00:35:53 +0800

    OTA未验证

--- lib/pages/device/iotDevice/subDeviceType/commWidgets/uploadOTA.dart
+++ lib/pages/device/iotDevice/subDeviceType/commWidgets/uploadOTA.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:path/path.dart' as path;
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class UploadOTAPage extends StatefulWidget {
   UploadOTAPage({Key key, this.url}) : super(key: key);
@@ -15,6 +17,8 @@ class _UploadOTAPageState extends State<UploadOTAPage> {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController _url_controller = TextEditingController.fromValue(
+        TextEditingValue(text: "http://192.168.0.2/ota.bin"));
     return Scaffold(
         body: Center(
           child: Container(
@@ -25,16 +29,14 @@ class _UploadOTAPageState extends State<UploadOTAPage> {
                   Container(height: 10),
                   Container(
                     child: TextField(
-                      controller: null,
-                      decoration: InputDecoration(labelText: '固件路径'),
+                      controller: _url_controller,
+                      decoration: InputDecoration(labelText: '固件网址'),
                     ),
                   ),
                   RaisedButton(
                     child: Text('开始更新'),
                     onPressed: () {
-                      setState(() {
-
-                      });
+                      _UploadBinFile(_url_controller.text);
                     },
                   ),
                 ],
@@ -43,7 +45,17 @@ class _UploadOTAPageState extends State<UploadOTAPage> {
       );
   }
 
-  _UploadBinFile(String path) async {
+  _UploadBinFile(String fromUrl) async {
+    Directory tempDir = await getTemporaryDirectory();
+    String localBinPath = path.join(tempDir.path,"ota.bin");
+    bool exist = await File(localBinPath).exists();
+    if (exist) {
+      await File(localBinPath).delete();
+    }
+    var downResponse = await Dio().download(fromUrl, localBinPath);
+    if (downResponse.statusCode != 200){
+      return;
+    }
     if(uploading){
       return;
     }
@@ -52,14 +64,50 @@ class _UploadOTAPageState extends State<UploadOTAPage> {
       widget.url = "/update";
     }
     FormData formData = FormData.from({
-      "update": UploadFileInfo(File(path), "ota.bin")
+      "update": UploadFileInfo(File(localBinPath), "ota.bin")
     });
     var response = await Dio().post(widget.url, data: formData);
     uploading = false;
     if (response.statusCode == 200 && jsonDecode(response.data)["code"] == 0){
 //      成功
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+              title: Text("设置名称："),
+              content: ListView(
+                children: <Widget>[
+                  Text("更新成功！"),
+                ],
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("确定"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ]),
+      );
     }
 //      失败
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+          title: Text("设置名称："),
+          content: ListView(
+            children: <Widget>[
+              Text("更新失败！"),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("确定"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ]),
+    );
   }
 
 }
