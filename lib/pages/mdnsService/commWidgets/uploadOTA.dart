c99a80e939f285ff8d604f595753364751bf1d69
Farry <newfarry@126.com>
2020-03-20 22:48:11 +0800

    del dio

--- lib/pages/mdnsService/commWidgets/uploadOTA.dart
+++ lib/pages/mdnsService/commWidgets/uploadOTA.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
-import 'dart:io';
+//import 'dart:io';
 import 'package:path/path.dart' as path;
-import 'package:dio/dio.dart';
+//import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -48,92 +48,92 @@ class _UploadOTAPageState extends State<UploadOTAPage> {
   }
 
   _UploadBinFile(String fromUrl) async {
-    Directory tempDir = await getTemporaryDirectory();
-    String localBinPath = path.join(tempDir.path, "ota.bin");
-    bool exist = await File(localBinPath).exists();
-    if (exist) {
-      await File(localBinPath).delete();
-    }
-    var downResponse = await Dio().download(fromUrl, localBinPath);
-    if (downResponse.statusCode != 200 ||
-        uploading ||
-        widget.url == null ||
-        widget.url == "") {
-      showDialog(
-        context: context,
-        builder: (_) => AlertDialog(
-            title: Text("OTA结果："),
-            content: Container(
-              height: 50,
-              child: ListView(
-                children: <Widget>[
-                  Text("下载设备程序失败！结果：${downResponse.statusCode}"),
-                ],
-              ),
-            ),
-            actions: <Widget>[
-              FlatButton(
-                child: Text("确定"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ]),
-      );
-      return;
-    }
-    uploading = true;
-    FormData formData = FormData.from(
-        {"update": UploadFileInfo(File(localBinPath), "ota.bin")});
-    var response = await Dio().post(widget.url, data: formData);
-    uploading = false;
-    await Navigator.of(context).pop();
-    if (response.statusCode == 200 && jsonDecode(response.data)["code"] == 0) {
-//      成功
-      showDialog(
-        context: context,
-        builder: (_) => AlertDialog(
-            title: Text("OTA结果："),
-            content: Container(
-              height: 30,
-              child: ListView(
-                children: <Widget>[
-                  Text("更新成功！"),
-                ],
-              ),
-            ),
-            actions: <Widget>[
-              FlatButton(
-                child: Text("确定"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ]),
-      );
-    } else {
-      //      失败
-      showDialog(
-        context: context,
-        builder: (_) => AlertDialog(
-            title: Text("OTA结果："),
-            content: Container(
-              height: 30,
-              child: ListView(
-                children: <Widget>[
-                  Text("更新失败！"),
-                ],
-              ),
-            ),
-            actions: <Widget>[
-              FlatButton(
-                child: Text("确定"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ]),
-      );
-    }
+//    Directory tempDir = await getTemporaryDirectory();
+//    String localBinPath = path.join(tempDir.path, "ota.bin");
+//    bool exist = await File(localBinPath).exists();
+//    if (exist) {
+//      await File(localBinPath).delete();
+//    }
+//    var downResponse = await Dio().download(fromUrl, localBinPath);
+//    if (downResponse.statusCode != 200 ||
+//        uploading ||
+//        widget.url == null ||
+//        widget.url == "") {
+//      showDialog(
+//        context: context,
+//        builder: (_) => AlertDialog(
+//            title: Text("OTA结果："),
+//            content: Container(
+//              height: 50,
+//              child: ListView(
+//                children: <Widget>[
+//                  Text("下载设备程序失败！结果：${downResponse.statusCode}"),
+//                ],
+//              ),
+//            ),
+//            actions: <Widget>[
+//              FlatButton(
+//                child: Text("确定"),
+//                onPressed: () {
+//                  Navigator.of(context).pop();
+//                },
+//              )
+//            ]),
+//      );
+//      return;
+//    }
+//    uploading = true;
+//    FormData formData = FormData.from(
+//        {"update": UploadFileInfo(File(localBinPath), "ota.bin")});
+//    var response = await Dio().post(widget.url, data: formData);
+//    uploading = false;
+//    await Navigator.of(context).pop();
+//    if (response.statusCode == 200 && jsonDecode(response.data)["code"] == 0) {
+////      成功
+//      showDialog(
+//        context: context,
+//        builder: (_) => AlertDialog(
+//            title: Text("OTA结果："),
+//            content: Container(
+//              height: 30,
+//              child: ListView(
+//                children: <Widget>[
+//                  Text("更新成功！"),
+//                ],
+//              ),
+//            ),
+//            actions: <Widget>[
+//              FlatButton(
+//                child: Text("确定"),
+//                onPressed: () {
+//                  Navigator.of(context).pop();
+//                },
+//              )
+//            ]),
+//      );
+//    } else {
+//      //      失败
+//      showDialog(
+//        context: context,
+//        builder: (_) => AlertDialog(
+//            title: Text("OTA结果："),
+//            content: Container(
+//              height: 30,
+//              child: ListView(
+//                children: <Widget>[
+//                  Text("更新失败！"),
+//                ],
+//              ),
+//            ),
+//            actions: <Widget>[
+//              FlatButton(
+//                child: Text("确定"),
+//                onPressed: () {
+//                  Navigator.of(context).pop();
+//                },
+//              )
+//            ]),
+//      );
+//    }
   }
 }
