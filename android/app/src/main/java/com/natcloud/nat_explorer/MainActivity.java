bd62d71c17323bdd16c515742040437e41b93fd4
Farry <newfarry@126.com>
2020-03-26 22:21:09 +0800

    aar 集成到主项目

--- android/app/src/main/java/com/natcloud/nat_explorer/MainActivity.java
+++ android/app/src/main/java/com/natcloud/nat_explorer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.natcloud.nat_explorer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import io.flutter.app.FlutterActivity;
@@ -9,6 +10,10 @@ public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//      add by Farry
+        Intent it=new Intent(this, OpenIoTHubBackgroundService.class);
+        this.startService(it);
+//      add by Farry over
         GeneratedPluginRegistrant.registerWith(this);
     }
 }
