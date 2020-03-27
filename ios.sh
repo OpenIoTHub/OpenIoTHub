59393ac0463e286dad3c9856999593645beeca6f
iotserv <yu@iotserv.com>
2020-03-28 01:16:21 +0800

    iOS苹果ipa程序打包

--- ios.sh
+++ ios.sh
@@ -1,1 +1,7 @@
-flutter build ios
+rm -rf Runner
+flutter build ios --debug --no-codesign
+mkdir Runner
+mkdir Runner/Payload
+cp -r build/ios/iphoneos/Runner.app Runner/Payload/Runner.app
+cd Runner
+zip -r Runner.ipa Payload
