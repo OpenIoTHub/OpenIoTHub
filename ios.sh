577b03618fb7597fdbd9e73ec814944d8998cb31
iotserv <yu@iotserv.com>
2020-03-28 21:25:00 +0800

    ipa pack

--- ios.sh
+++ ios.sh
@@ -1,7 +1,9 @@
 rm -rf Runner
-flutter build ios --debug --no-codesign
+#flutter build ios --debug --no-codesign
+flutter build ios
 mkdir Runner
 mkdir Runner/Payload
 cp -r build/ios/iphoneos/Runner.app Runner/Payload/Runner.app
 cd Runner
 zip -r Runner.ipa Payload
+open Payload
