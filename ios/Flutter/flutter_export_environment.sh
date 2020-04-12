9da63f70b05c5f150a162ae5c2138dd179d36fc3
Farry <newfarry@126.com>
2020-04-13 01:38:57 +0800

    update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\git\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Farry\StudioProjects\nat_explorer"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Farry\StudioProjects\OpenIoTHub"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
