37824275f92b713ee2fa52e1eda89f85ed760eb1
Yu Fang <yu@iotserv.com>
2021-06-30 13:42:42 +0800

    添加微信绑定和微信登录

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
+flutter build apk -v
+open build/app/outputs/flutter-apk
 flutter build apk --split-per-abi -v
 ls build/app/outputs/flutter-apk
-open build/app/outputs/flutter-apk
-flutter build apk -v
