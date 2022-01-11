110bb6faad7c794a3fdb8399b4f6f38aac78e200
Yu Fang <yu@iotserv.com>
2022-01-12 00:30:40 +0800

    upgrade lib

--- android/app/src/main/java/com/iotserv/openiothub/MainActivity.java
+++ android/app/src/main/java/com/iotserv/openiothub/MainActivity.java
@@ -7,11 +7,15 @@ import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//      add by Farry
-        Intent it=new Intent(this, OpenIoTHubBackgroundService.class);
-        this.startService(it);
-//      add by Farry over
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            // Your existing code
+                            Intent it = new Intent(this, OpenIoTHubBackgroundService.class);
+                            this.startService(it);
+                        }
+                );
     }
 }
