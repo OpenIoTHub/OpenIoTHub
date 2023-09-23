a6e89de548fbb29139a9ff604a1ceaf746840f11
iotserv <newfarry@126.com>
2023-09-23 18:56:11 +0800

    pub upgrade

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,7 +25,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(100, 100);
+  Win32Window::Point origin(500, 150);
   Win32Window::Size size(500, 720);
   if (!window.Create(L"云亿连", origin, size)) {
     return EXIT_FAILURE;
