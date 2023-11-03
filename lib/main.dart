3081e1c70f559729676561e3916475ef03ff83fc
iotserv <newfarry@126.com>
2023-11-03 21:32:53 +0800

    SplashPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:openiothub/generated/l10n.dart';
 import 'package:openiothub/init.dart';
 import 'package:openiothub/model/custom_theme.dart';
+import 'package:openiothub/pages/homePage/homePage.dart';
 import 'package:openiothub/pages/splashPage/splashPage.dart';
 import 'package:provider/provider.dart';
 import 'package:oktoast/oktoast.dart';
@@ -24,22 +27,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return OKToast(child: MaterialApp(
-        title: "OpenIoTHub",
-        theme: CustomThemes.light,
-        darkTheme: CustomThemes.dark,
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: const [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate
-        ],
-        supportedLocales: S.delegate.supportedLocales,
-        localeListResolutionCallback: (locales, supportedLocales) {
-          print("locales:$locales");
-          return;
-        },
-        home: SplashPage()));
+    return OKToast(
+        child: MaterialApp(
+            title: "OpenIoTHub",
+            theme: CustomThemes.light,
+            darkTheme: CustomThemes.dark,
+            debugShowCheckedModeBanner: false,
+            localizationsDelegates: const [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            localeListResolutionCallback: (locales, supportedLocales) {
+              print("locales:$locales");
+              return;
+            },
+            home: (Platform.isAndroid || Platform.isIOS)
+                ? SplashPage()
+                : MyHomePage(
+                    key: UniqueKey(),
+                    title: '',
+                  )));
   }
 }
