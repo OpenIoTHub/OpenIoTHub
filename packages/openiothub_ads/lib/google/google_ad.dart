bcd97f964a8de7a04b8b9a279a1508f91323615b
Yu Fang <newfarry@126.com>
2025-08-07 21:01:42 +0800

    admob banner

--- packages/openiothub_ads/lib/google/google_ad.dart
+++ packages/openiothub_ads/lib/google/google_ad.dart
@@ -1,3 +1,4 @@
+export 'package:google_mobile_ads/google_mobile_ads.dart';
 export 'app_open/app_open_ad_manager.dart';
 export 'common/consent_manager.dart';
 export 'rewarded/countdown_timer.dart';
