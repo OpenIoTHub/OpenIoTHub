e32d9f261a506eeb99b47a90683415de61ab8b33
Yu Fang <newfarry@126.com>
2026-01-11 15:31:51 +0800

    update:ad config

--- packages/openiothub_ads/lib/configs/configs.dart
+++ packages/openiothub_ads/lib/configs/configs.dart
@@ -1,7 +1,7 @@
-// export 'google_config.dart';
-export 'google_config_no_commit.dart';
-// export 'csj_config.dart';
-export 'csj_config_no_commit.dart';
-// export 'ylh_config.dart';
-export 'ylh_config_no_commit.dart';
+export 'google_config.dart';
+// export 'google_config_no_commit.dart';
+export 'csj_config.dart';
+// export 'csj_config_no_commit.dart';
+export 'ylh_config.dart';
+// export 'ylh_config_no_commit.dart';
 // export 'ylh_config_juhe_no_commit.dart';
